5a48eea879a58a458acee6532426446d929a4d1c
FxPool <111183867+FxPool@users.noreply.github.com>
2023-05-12 15:31:33 +0800

    Update install_zh.sh

--- install_zh.sh
+++ install_zh.sh
@@ -1,6 +1,6 @@
 #bin
 version='10.3.1'
-shell_version='4.1'
+shell_version='5.0.0'
 uiname='FXMinerProxyV3-shell'
 pkgname='FXMinerProxy'
 authorname='FxPool'
@@ -34,6 +34,45 @@ for i in $PROCESS; do
     echo "Kill the $1 process [ $i ]"
     kill -9 $i
 done
+
+OsSupport()
+{
+    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    else
+        DISTRO='unknow'
+    fi
+    echo $DISTRO;
+    str1="CentOS,Ubuntu,Debian"
+    if [[ $str1 =~ $DISTRO ]]
+    then
+       # echo support this os system 
+       return
+    else
+       # echo not support this os system pls use CentOS,Ubuntu,Debian
+       echo -e "${red}不支持的操作系统，请使用CentOS或Ubuntu或Debian"
+       return
+    fi
+}
 
 change_limit() {
     changeLimit="n"
@@ -101,6 +140,7 @@ kill_wdog(){
 }
 
 install() {
+    OsSupport
     if [ ! -f "$installfolder" ]; then
         wget https://github.com/$authorname/$pkgname/archive/refs/tags/$version.tar.gz
         if [ -f "$version.tar.gz" ]; then
@@ -253,27 +293,39 @@ stop() {
     before_show_menu
 }
 autorun() {
-    cd /etc
-    rm rc.local
-    touch rc.local
-    chmod 777 rc.local
-    echo "#!/bin/bash" >>rc.local
-    echo "#" >>rc.local
-    echo "# rc.local" >>rc.local
-    echo "#" >>rc.local
-    echo "# This script is executed at the end of each multiuser runlevel." >>rc.local
-    echo "# Make sure that the script will " #exit 0" on success or any other" >> rc.local
-    echo "# value on error." >>rc.local
-    echo "#" >>rc.local
-    echo "# In order to enable or disable this script just change the execution" >>rc.local
-    echo "# bits." >>rc.local
-    echo "#" >>rc.local
-    echo "# By default this script does nothing." >>rc.local
-    echo "#exit 0" >>rc.local
-    echo "cd $installdir && setsid ./$wdog &" >>rc.local
-    echo "exit 0" >>rc.local
-    cd /root
-    echo -e "${green}开机启动设置成功"
+    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        cd /etc/rc.d/
+        rm rc.local
+        touch rc.local
+        chmod 777 rc.local
+        echo "#!/bin/bash" >>rc.local
+        echo "cd $installdir && setsid ./$wdog &" >>rc.local
+        echo "exit 0" >>rc.local
+        cd /root
+        echo -e "${green}开机启动设置成功"
+    else
+        cd /etc
+        rm rc.local
+        touch rc.local
+        chmod 777 rc.local
+        echo "#!/bin/bash" >>rc.local
+        echo "#" >>rc.local
+        echo "# rc.local" >>rc.local
+        echo "#" >>rc.local
+        echo "# This script is executed at the end of each multiuser runlevel." >>rc.local
+        echo "# Make sure that the script will " #exit 0" on success or any other" >> rc.local
+        echo "# value on error." >>rc.local
+        echo "#" >>rc.local
+        echo "# In order to enable or disable this script just change the execution" >>rc.local
+        echo "# bits." >>rc.local
+        echo "#" >>rc.local
+        echo "# By default this script does nothing." >>rc.local
+        echo "#exit 0" >>rc.local
+        echo "cd $installdir && setsid ./$wdog &" >>rc.local
+        echo "exit 0" >>rc.local
+        cd /root
+        echo -e "${green}开机启动设置成功"
+    fi
 }
 closeWhiteList(){
     cd $installdir
