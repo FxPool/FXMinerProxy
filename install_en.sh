054fa430aabac50fb8e59ea2533dd4f098170d73
FxPool <hansenloveyou520@gmail.com>
2023-10-21 04:49:00 +0800

    12.1.2
    FIX:KSAPA币种k1矿池协议优化现在可以兼容所有矿池了

--- install_en.sh
+++ install_en.sh
@@ -1,5 +1,5 @@
 #bin
-version='12.1.1'
+version='12.1.2'
 shell_version='5.1'
 uiname='FXMinerProxyV3-shell'
 pkgname='FXMinerProxy'
--- install_zh.sh
+++ install_zh.sh
@@ -2,7 +2,7 @@
 
 router_line=$1
 
-version='12.1.1'
+version='12.1.2'
 shell_version='6.0.5'
 uiname='FXMinerProxyV3-shell'
 pkgname='FXMinerProxy'
